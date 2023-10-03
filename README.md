d0f5d0ef7325f58825c63faa7fd6349de35bd90c
juanbyluna <juan.fernandes@alunoescolassociais.org.br>
2023-10-03 12:07:48 -0300

    adicionado gif e estrutura do README

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Form-CadEndereco
+# Formulario de Cadastro de Endereço
+Projeto criado em sala de aula com ajuda do Professor Leonardo Santiago Sidon da Rocha.
+
+* [Projeto](#Formulario-de-Cadastro-de-Endereco)  
+* [Capa-do-Projeto]
+* [Descrição](#descrição)    
+* [Funcionalidades](#funcionalidades)  
+* [Tecologias utilizadas](#tecologias-utilizadas)  
+* [Fontes consultadas](#fontes-consultadas)  
+* [Autores](#autores)
+
+## 🖼️Capa do Projeto
+<img src="imgs/Capa.png">
+
+## 📄Descrição 
+
+O prjeto consiste em um calendario virtual que qualquer pessoa pode acessar atravez de seu smartphone ou computador.
+(Possui apenas um mes)
+## 👾Funcionalidades
+Escolher cor para adicionar em um dia especifico do mês;
+Selecionar data;
+Aplicar cor a data selecionada;
+## 🔧Como usar
+<img src="imgs/Comousar.gif">
+
+##### Tabela de cores
+* 💙Azul
+* 💚Verde 
+* 💗Rosa 
+* 💜Roxo  
+
+### 🤖Tecologias utilizadas
+* HTML 5
+* CSS 3
+* VsCode (Visual Code)
+
+## 🔧Ferramentas utilizadas
+* JavaScript
+* GitHub
+## 🔗Fontes consultadas
+* Chat gpt
+* Google
+* https://www.alura.com.br/
+
+## 🤜🤛Colaboradores
+Leonardo Santiago Sidon da Rocha.
+
+## ✒️Autores
+Juan Fernandes Goes de Souza
--- form.js
+++ form.js
@@ -1,45 +1,45 @@
-'use strict';//moodo restrito
+'user strict'; //Modo strict mode ou Modo restrito
 //https://viacep.com.br/
-
-//função para limpar formulario
-const limparFormulario = (endereco) => {
-    document.getElementById('rua').value='';
-    document.getElementById('bairro').value='';
-    document.getElementById('cidade').value='';
-    document.getElementById('estado').value='';
-
+ 
+//Função para limpar formulário
+//Arrow function
+const limparformulario = () => {
+      document.getElementById('rua').value = '';
+      document.getElementById('bairro').value = '';
+      document.getElementById('cidade').value = '';
+      document.getElementById('estado').value = '';
+ 
 }
-// verifica se CEP é valido
-const eNumero = (numero) => /^[0-9]+$/.test (numero);
-const cepValido = (cep) => cep.length = 8 && eNumero(cep);
-
+ 
+//Verifica se CEP é válido
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+ 
 //Responsável pelo preenchimento do formulário
 const preencherFormulario = (endereco) => {
-    document.getElementById('rua').value=endereco.logradouro;
-    document.getElementById('bairro').value=endereco.bairro;
-    document.getElementById('cidade').value=endereco.localidade;
-    document.getElementById('estado').value=endereco.uf;  
+      document.getElementById('rua').value = endereco.logradouro;
+      document.getElementById('bairro').value = endereco.bairro;
+      document.getElementById('cidade').value = endereco.localidade;
+      document.getElementById('estado').value = endereco.uf;
 }
-
-//função para consumo de API da Via CEP
-const perquisarCep = async() => { 
-    limparFormulario();
-    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
-
-    if(cepValido(cep.value)){
-        const dados = await fetch(url);// esperar
-        const address = await dados.json(); //retorna dados no formato JSON
-
-        if(address.hasOwnProperty('erro')){
-            alert('CEP não encontrado');
-        }else{
-            preencherFormulario;(address);
-        }
-         }else{
-            alert('CEP incorreto')     
-         
-    }
+ 
+// Função para consumo de API da via CEP
+const pesquisarCep = async() => {
+      limparformulario();
+      const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+ 
+      if (cepValido(cep.value)) {
+            const dados = await fetch(url); //esperar
+            const address = await dados.json(); //retorna dados no formato JSON
+ 
+            if (address.hasOwnProperty('erro')) {
+                  alert('CEP não encontrado');
+            } else {
+                  preencherFormulario(address);
+            }
+      } else {
+            alert('CEP incorreto');
+      }
 }
-
-// adiciona em evento DOM no input do CEP
-document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+      //Adiciona um evento DOM no input do CEP
+      document.getElementById('cep').addEventListener('focusout', pesquisarCep)
